b9ff6dd613f2e2f0b9783ca05835686cd7d0d7ac
manojv200 <manoj.vkumar200@gmail.com>
2025-01-24 19:13:33 +0530

    Added new files.

--- schedules/models.py
+++ schedules/models.py
@@ -4,8 +4,14 @@ from django.db import models
 
 # Create your models here.
 class TblUsers(AbstractUser):
+    Interviewer = 1
+    Candidate = 2
+    type_choices = (
+        (Interviewer, 'Interviewer'),
+        (Candidate, 'Candidate'),
+    )
     user_type_id = models.CharField(null=True, blank=True, max_length=255)
-    user_type = models.CharField(null=True, blank=True, max_length=255)
+    user_type = models.CharField(null=True, blank=True, max_length=255,choices=type_choices)
     start_date = models.IntegerField(null=True, blank=True)
     end_date = models.IntegerField(null=True, blank=True)
 
